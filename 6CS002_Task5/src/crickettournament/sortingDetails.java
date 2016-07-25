09124aaf98f3fbdb074a44f6b249f561f6f06643
washani <kiaraduva@gmail.com>
2016-07-25 06:04:41 +0530

    Find the lowest number of the Won matches and total points 1000 and 1500 teams print.

--- 6CS002_Task5/src/crickettournament/sortingDetails.java
+++ 6CS002_Task5/src/crickettournament/sortingDetails.java
@@ -11,10 +11,10 @@ public class sortingDetails {
 
 	public void lowest(){
 		List<cricketSystem> table = (List<cricketSystem>) Arrays.asList(
-				new cricketSystem(1, "SriLanka", 12,20,10),
-				new cricketSystem(2, "India   ", 12,15,5),
-				new cricketSystem(3, "Pakistan", 12, 16,12),
-				new cricketSystem(4, "America", 12, 20, 15)
+				new cricketSystem(1, "SriLanka", 12,20,10,1002),
+				new cricketSystem(2, "India   ", 12,15,5,1500),
+				new cricketSystem(3, "Pakistan", 12, 16,12,1200),
+				new cricketSystem(4, "America", 12, 20, 15,1000)
 				
 			//new cricketSystem(1, "SriLanka", 22, 16, 1, 5, 621, 400, 221, 75, 41, 8, 2, 76, 4, 67,67,22,45,32,12),
 			//new cricketSystem(2, "India", 22, 16, 1, 5, 621, 400, 221, 75, 41, 8, 2, 76, 4, 67,67,22,45,32,12),
@@ -27,5 +27,14 @@ public class sortingDetails {
 	} else {
 	System.out.println("min failed");
 	}
-	}
+	
+	System.out.println("Several Teams have 1000 points");
+	 table.stream().filter(cricketSystem -> cricketSystem.getTotalTestPoints() == 1000)
+	 .forEach(System.out::println);
+
+	 System.out.println();
+	 System.out.println("Teams with 1500 points");
+	 table.stream().filter(cricketSystem -> cricketSystem.getTotalTestPoints() == 1500)
+	 .forEach(System.out::println);
+ }
 }
